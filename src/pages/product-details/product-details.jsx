3cda5e66c491c1835adba718144c5ad896907b4e
DSantosDen <51485458+DSantosDen@users.noreply.github.com>
2024-07-22 20:49:03 -0600

    Additional edits to the UI

--- src/pages/product-details/product-details.jsx
+++ src/pages/product-details/product-details.jsx
@@ -1,4 +1,39 @@
-function ProductDetail() {
-  return <h1>Product Details</h1>;
-}
-export default ProductDetail;
+const product = {
+  id: 1,
+  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  price: 109.95,
+  description:
+    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  category: "men's clothing",
+  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  rating: {
+    rate: 3.9,
+    count: 120,
+  },
+};
+
+const ProductDetails = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <img className="w-full" src={product.image} alt={product.title} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{product.title}</div>
+        <p className="text-gray-700 text-base">{product.description}</p>
+        <div className="font-bold text-lg mb-2">${product.price}</div>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Category: {product.category}
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Rating: {product.rating.rate}
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Reviews: {product.rating.count}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
