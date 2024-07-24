68d6b9467758265d78f729934c1a80b8bb2b9ad8
DSantosDen <51485458+DSantosDen@users.noreply.github.com>
2024-07-24 11:40:07 -0600

    continuining editing the ui and initial tests for sorting

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Card from "../../components/cards/cards";
 import axios from "axios";
+import SortPopup from "../../components/sort-popup/sort-popup";
 
 function Home() {
+  const [ogdata, setOgData] = useState([]);
   const [data, setData] = useState([]);
   useEffect(() => {
     fetchData();
@@ -11,19 +13,43 @@ function Home() {
   const fetchData = async () => {
     let res = await axios.get("https://fakestoreapi.com/products");
     setData(res.data);
+    setOgData(res.data);
   };
+
+  function customeSort(sortBy, sortType) {
+    let sortedData = [];
+    if (typeof ogdata[0][sortBy] == "number") {
+      if (sortType == 1) {
+        sortedData = ogdata.sort((a, b) => b[sortBy] - a[sortBy]);
+      } else {
+        sortedData = ogdata.sort((a, b) => a[sortBy] - b[sortBy]);
+      }
+    } else {
+      if (sortType == 1) {
+        sortedData = ogdata.sort((a, b) => b[sortBy] < a[sortBy]);
+      } else {
+        sortedData = ogdata.sort((a, b) => a[sortBy] < b[sortBy]);
+      }
+    }
+    console.log(sortedData, sortBy);
+    setData([...sortedData]);
+  }
+
   return (
-    <div className="flex flex-wrap mx-auto justify-center">
-      {data.map((item) => (
-        <Card
-          key={item.id}
-          id={item.id}
-          img={item.image}
-          price={item.price}
-          title={item.title}
-        />
-      ))}
-    </div>
+    <>
+      <SortPopup customeSort={customeSort} />
+      <div className="flex flex-wrap mx-auto justify-center">
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            id={item.id}
+            img={item.image}
+            price={item.price}
+            title={item.title}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
