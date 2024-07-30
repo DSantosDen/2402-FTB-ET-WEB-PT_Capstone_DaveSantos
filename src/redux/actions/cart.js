cadb05349153d2c8e5a9e569086085923d74bbdf
DSantosDen <51485458+DSantosDen@users.noreply.github.com>
2024-07-29 19:41:20 -0600

    Initial implementation of JWT (JSON Web Token)

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,3 +1,9 @@
+/*imports from redux toolkit. 
+Redux offers centralized state management, predictable state updates, 
+easier debugging with Redux DevTools, and improved performance by
+ minimizing state updates and reducing prop drilling.
+ READ: https://www.freecodecamp.org/news/redux-and-redux-toolkit-for-beginners/
+*/
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
+//imports from redux store and cart
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./actions/cart";
 
+/*the Redux store is created by the {configureStore} import
+The store is configured by the cartReducer, which will handle all 
+updates related to the cart slice of the state.*/
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
